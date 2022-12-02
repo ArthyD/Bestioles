0dc2d78c5e7a8812c2a3f7901e4ded2ed37be9e9
ArthurD <arthur.docquois@wanadoo.fr>
2022-12-02 12:06:48 +0100

    Bestiole prevoyante

--- BestiolePrevoyante.cpp
+++ BestiolePrevoyante.cpp
@@ -72,15 +72,18 @@ void BestiolePrevoyante::update(vector<Bestiole*>& vectorBestioleProche){
     for (auto & bestioleProche : vectorBestioleProche){
         mauvaisesOrientations.push_back(bestioleProche->getOrientation());
     }
+
     std::sort (mauvaisesOrientations.begin(), mauvaisesOrientations.end());
     double meilleurEcart = 0;
-    double meilleureOrientation = getOrientation();
-    for (std::vector<double>::iterator it = mauvaisesOrientations.begin() ; it!= mauvaisesOrientations.end() - 1 ; ++it) {
-        double ecart = *(it+1) - *it;
+    double meilleureOrientation = orientation;
+    double ecart;
+    for (std::vector<double>::iterator it = mauvaisesOrientations.begin() ; it!= mauvaisesOrientations.end(); ++it) {
+        ecart = *(it+1) - *it;
+
         if (ecart > meilleurEcart) {
             meilleurEcart = ecart;
             meilleureOrientation = (*(it) + *(it+1)) / 2;
         }
     }
-    setOrientation(meilleureOrientation);
+    orientation = meilleureOrientation;
 }
