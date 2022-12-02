98b5fc3e88c5d368064f3e8a8f1da2f696fcdbe5
Danaelmilo <98761954+Danaelmilo@users.noreply.github.com>
2022-12-02 11:24:03 +0100

    Add files via upload
    on prévoit

--- BestiolePrevoyante.cpp
+++ BestiolePrevoyante.cpp
@@ -1,26 +1,21 @@
 #include "BestiolePrevoyante.h"
-#include <iostream>
-BestiolePrevoyante::BestiolePrevoyante(bool persoMult, int age, double champOeil, double distanceOeil, double distanceOreille, double capaciteOeil, double capaciteOreille, double coeffCamouflage, double coeffCarapace, double coeffNageoire){
-    std::cout << "Create bestiole prevoyante" << std::endl;
-}  
+
 void BestiolePrevoyante::update(vector<Bestiole*> vectorBestioleProche){
 
-    // vector<double> mauvaisesOrientations;
+    vector<double> mauvaisesOrientations;
 
-    // for (auto & bestioleProche : vectorBestioleProche){
-    //     mauvaisesOrientations.add(bestioleProche.getOrientation())
-    // }
-
-    // double meilleurEcart = 0;
-    // double meilleureOrientation = this.getOrientation();
-    // for (int i=0; i<mauvaisesOrientations.size(); i++){
-    //     for (int j=i; j<mauvaisesOrientations.size(); i++){
-    //         double ecart = abs(mauvaisesOrientations.get(i) - mauvaisesOrientations.get(j));
-    //         if (ecart > meilleurEcart){
-    //             meilleurEcart = ecart;
-    //             double meilleureOrientation = (mauvaisesOrientations.get(i) + mauvaisesOrientations.get(j)) / 2;
-    //         }
-    //     }
-    // }
-    // setOrientation(meilleureOrientation);
+    for (auto & bestioleProche : vectorBestioleProche){
+        mauvaisesOrientations.push_back(bestioleProche->getOrientation());
+    }
+    std::sort (mauvaisesOrientations.begin(), mauvaisesOrientations.end());
+    double meilleurEcart = 0;
+    double meilleureOrientation = getOrientation();
+    for (std::vector<double>::iterator it = mauvaisesOrientations.begin() ; it!= mauvaisesOrientations.end() - 1 ; ++it) {
+        double ecart = *(it+1) - *it;
+        if (ecart > meilleurEcart) {
+            meilleurEcart = ecart;
+            meilleureOrientation = (*(it) + *(it+1)) / 2;
+        }
+    }
+    setOrientation(meilleureOrientation);
 }
