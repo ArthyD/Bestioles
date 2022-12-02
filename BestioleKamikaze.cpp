926e7f02172396f1ad0625458c24716fb3e3878b
TanatornG <nicolas.sempere@free.fr>
2022-12-02 11:23:42 +0100

    Suppression de gregaire.h et kamikaze.h

--- BestioleKamikaze.cpp
+++ BestioleKamikaze.cpp
@@ -1,22 +1,38 @@
 #include "BestioleKamikaze.h"
+#include "Bestiole.h"
 #include <cmath>
 
 double highValue = 300000;
+double cibleX = 0;
+double cibleY = 0;
+
+// Fonction update 
+// Entrée : listes des bestioles
+// Sortie : rien
+// Actions : détermine quelle est la bestiole la plus proche de la
+//           bestiole courante. Actualise l'orientation de la bestiole
+//           bestiole courante pour qu'elle se dirige vers la bestiole
+//           la plus proche.
 
 void BestioleKamikaze::update(vector<Bestiole*> vectorBestioleProche){
     if (vectorBestioleProche.size() > 0){
         double meilleureDistanceCarre = highValue;
-        for (auto & bestioleProche : vectorBestioleProche){
-            double deltaX = bestioleProche.getX() - this.getX();
-            double deltaY = bestioleProche.getY() - this.getY();
+        for ( std::vector<Bestiole*>::iterator it1 = vectorBestioleProche.begin();
+        it1 != vectorBestioleProche.end() ; it1++ ){
+            
+            double deltaX = (**it1).getX() - x;
+            double deltaY = (**it1).getY() - y;
             double distanceCarre = deltaX*deltaX + deltaY*deltaY;
+
             if (distanceCarre < meilleureDistanceCarre){
                 meilleureDistanceCarre = distanceCarre;
-                double cible = bestioleProche;
+                cibleX = (**it1).getX();
+                cibleY = (**it1).getY();
             }
         }
-        if (deltaX < 0){
-            double orientation = atan(deltaX / deltaY);
-        }
+
+        double deltaX = cibleX - x;
+        double deltaY = cibleY - y;
+        double orientation = atan(deltaY / deltaX);
     }
 }
